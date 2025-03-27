3c960070221c0482f7db6dca7d6b2c1c5ecff8ed
MGTolentino <mgtolentino97@gmail.com>
2025-03-27 10:15:25 -0600

    Cambiar bucket

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
-// Configuración de tu proyecto Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyAhDO33NidZrb56OTrObx7Lj2xupjDkdfM",
-    authDomain: "xv-caro-3532a.firebaseapp.com",
-    projectId: "xv-caro-3532a",
-    storageBucket: "xv-caro-3532a.appspot.com",
-    messagingSenderId: "64636102051",
-    appId: "1:64636102051:web:0fb6016c8d50309b4a2616"
-  };
+// Configuración de tu proyecto Firebase 
+const firebaseConfig = {  
+  apiKey: "AIzaSyAhDO33NidZrb56OTrObx7Lj2xupjDkdfM",
+  authDomain: "xv-caro-3532a.firebaseapp.com",
+  projectId: "xv-caro-3532a",
+  storageBucket: "xv-caro-3532a.firebasestorage.app", // CAMBIAR ESTA LÍNEA
+  messagingSenderId: "64636102051",
+  appId: "1:64636102051:web:0fb6016c8d50309b4a2616"
+};
   
   console.log('Antes de inicializar Firebase');
 
